841a50ddd178874151e686f7ddec1d39c04c1506
pythoncoderk <turntabroz@gmail.com>
2026-01-05 13:04:23 +0900

    atcoder

---
+++ 0105_3.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, k, m;
+    cin >> n >> k >> m;
+    int cnt = 0;
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        int check;
+        cin >> check;
+        sum += check;
+        if (check >= k) cnt += check;
+    }
+    if (cnt >= 3 && sum >= m) cout << "silver";
+    else cout << "bronze" << endl;
+
+}
