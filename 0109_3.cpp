a515ee50847cc43e6b2ec7a2a79a6d426cd8c950
pythoncoderk <turntabroz@gmail.com>
2026-01-09 15:30:58 +0900

    atcoder

--- 0109_3.cpp
+++ 0109_3.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
-    
+    int n, a;
+    cin >> n >> a;
+
+    for (int i = 0; i <= a; i++) {
+        if ((n - i) % 500 == 0) {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
 }
